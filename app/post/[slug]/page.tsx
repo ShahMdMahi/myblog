441b0db5755b67a4dfeeeaf4b90aed6e97648024
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-08 20:23:28 +0600

    first commit

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -2,20 +2,20 @@ import { Metadata } from "next";
 import PostDetail from "@/components/PostDetail";
 
 interface PostPageProps {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 }
 
 export async function generateMetadata({
   params,
 }: PostPageProps): Promise<Metadata> {
+  const slug = (await params).slug;
   // You would typically fetch the post data here to get the title
   return {
-    title: `Post: ${params.slug}`,
+    title: `Post: ${slug}`,
   };
 }
 
-export default function PostPage({ params }: PostPageProps) {
-  return <PostDetail slug={params.slug} />;
+export default async function PostPage({ params }: PostPageProps) {
+  const slug = (await params).slug;
+  return <PostDetail slug={slug} />;
 }
